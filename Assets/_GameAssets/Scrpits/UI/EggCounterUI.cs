b922ab7b3685f404bc65a41b03bb805859464a66
Latif Can <116440964+LatifCanDusgun@users.noreply.github.com>
2025-05-16 15:46:33 +0300

    v0.14 Win Condition Eggs & UI Added

---
+++ Assets/_GameAssets/Scrpits/UI/EggCounterUI.cs
@@ -0,0 +1,36 @@
+using System;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EggCounterUI : MonoBehaviour
+{
+   [Header("References")] 
+   [SerializeField] private TMP_Text _eggCounterText;
+
+   [Header("Settings")]
+   [SerializeField] private Color _eggCounterColor;
+
+   [SerializeField] private float _colorDuration;
+
+   [SerializeField] private float _scaleDuration;
+   
+   private RectTransform _eggCounterRectTransform;
+
+   private void Awake()
+   {
+      _eggCounterRectTransform = _eggCounterText.gameObject.GetComponent<RectTransform>();
+   }
+
+   public void SetEggCounterText(int counter, int max)
+   {
+      _eggCounterText.text = counter.ToString() + "/" + max.ToString();
+   }
+
+   public void SetEggCompleted()
+   {
+      _eggCounterText.color = _eggCounterColor;
+      _eggCounterRectTransform.DOScale(1.2f, _scaleDuration).SetEase(Ease.OutBack);
+   }
+}

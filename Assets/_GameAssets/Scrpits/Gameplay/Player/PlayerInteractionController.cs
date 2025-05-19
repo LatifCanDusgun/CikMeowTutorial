972562a3f291168e9ea357a480634cf14f6754bc
Latif Can <116440964+LatifCanDusgun@users.noreply.github.com>
2025-05-19 19:10:50 +0300

    v0.18 Damageable Fires Added

--- Assets/_GameAssets/Scrpits/Gameplay/Player/PlayerInteractionController.cs
+++ Assets/_GameAssets/Scrpits/Gameplay/Player/PlayerInteractionController.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 
 public class PlayerInteractionController : MonoBehaviour
 {
+     [SerializeField] private Transform _playerVisualTransform;
      
      private PlayerController _playerController;
+     
+     private Rigidbody _playerRigidbody;
 
      private void Awake()
      {
           _playerController = GetComponent<PlayerController>();
+          _playerRigidbody = GetComponent<Rigidbody>();
      }
 
      private void OnTriggerEnter(Collider other)
@@ -24,6 +28,14 @@ public class PlayerInteractionController : MonoBehaviour
           if (other.gameObject.TryGetComponent<IBoostables>(out var boostable ))
           {
                boostable.Boots(_playerController);
+          }
+     }
+
+     private void OnParticleCollision(GameObject other)
+     {
+          if (other.TryGetComponent<IDamageable>(out var damageable))
+          {
+               damageable.GiveDamge(_playerRigidbody, _playerVisualTransform);
           }
      }
 }
